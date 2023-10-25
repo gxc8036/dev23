a5be36c71da232b263231656a586ade26fe6e5f0
gus97 <19951799881@163.com>
2023-10-25 20:54:06 +0800

    init

---
+++ src/main/java/com/gus/dev23/Dev23Application.java
@@ -0,0 +1,13 @@
+package com.gus.dev23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dev23Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Dev23Application.class, args);
+    }
+
+}
